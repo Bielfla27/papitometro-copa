c36cc15ecde77128479b2aa63fb4e227fef68174
Bielfla27 <gabibielmft@outlook.com>
2026-06-15 22:09:36 -0300

    Libera CORS para frontend da Vercel

--- papitometroCopaDoMundo/src/main/java/copa/papitometroCopaDoMundo/config/SecurityConfig.java
+++ papitometroCopaDoMundo/src/main/java/copa/papitometroCopaDoMundo/config/SecurityConfig.java
@@ -48,7 +48,11 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://papitometro-copa.vercel.app"
+        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
